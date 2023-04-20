03ac4264465d20350efd1a870857e24dbd3e9ede
Alex <111201814+Alex666777666@users.noreply.github.com>
2023-04-20 18:27:32 +0300

    Update Profile.jsx

--- src/Profile.jsx
+++ src/Profile.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import UserForm from './UserForm.jsx'
+import React from 'react';
+import UserForm from './UserForm';
 
 const Profile = ({ userData, handleChange }) => {
   return (
-    <div className='column'>
+    <div className="column">
       <UserForm userData={userData} handleChange={handleChange} />
     </div>
-  )
-}
+  );
+};
 
-export default Profile
+export default Profile;
